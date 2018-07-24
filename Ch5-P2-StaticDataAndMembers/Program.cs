1a8e72226463072f2651d7c90b77ae8d6b17db3c
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-24 16:33:30 +0500

    first commit from lec

---
+++ Ch5-P2-StaticDataAndMembers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P2_StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ch5-P2-StaticDataAndMembers ");
+
+            Console.ReadLine();
+        }
+    }
+}
