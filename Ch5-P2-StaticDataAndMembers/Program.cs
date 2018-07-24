b1d0808047a0b1313bff6ffd7d2041938a2743bd
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-24 17:44:02 +0500

    final commit from lec 2 < files added >

--- Ch5-P2-StaticDataAndMembers/Program.cs
+++ Ch5-P2-StaticDataAndMembers/Program.cs
@@ -10,7 +10,51 @@ namespace Ch5_P2_StaticDataAndMembers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Ch5-P2-StaticDataAndMembers ");
+            Console.WriteLine("***** Fun with Static Data *****\n");
+
+            #region Defining Static Field Data
+
+            //SavingsAccount s1 = new SavingsAccount(50);
+            //SavingsAccount s2 = new SavingsAccount(100);
+            //SavingsAccount s3 = new SavingsAccount(10000.75);
+
+            #endregion
+
+
+            #region Defining Static Methods
+
+
+            //SavingsAccount s1 = new SavingsAccount(50);
+            //SavingsAccount s2 = new SavingsAccount(100);
+
+            //// Print the current interest rate.
+            //Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            //// Make new object, this does NOT 'reset' the interest rate.
+            //SavingsAccount s3 = new SavingsAccount(10000.75);
+            //Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            #endregion
+
+            #region Defining Static Constructors
+
+            // Make an account.
+            SavingsAccount s1 = new SavingsAccount(50);
+            
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            
+            // Try to change the interest rate via property.
+            SavingsAccount.SetInterestRate(0.08);
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            // Make a second account.
+            SavingsAccount s2 = new SavingsAccount(100);
+            
+            // Should print 0.08...right??
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            #endregion
 
             Console.ReadLine();
         }
---
+++ Ch5-P2-StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P2_StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        // Instance-level data.
+        public double currBalance;
+
+        // A static point of data.
+        //public static double currInterestRate;
+        public static double currInterestRate = 0.04; // member initializaiton syntax
+
+        // A static constructor!
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static constructor !");
+            currInterestRate = 0.04;
+        }
+
+        public SavingsAccount(double balance)
+        {
+           // currInterestRate = 0.04; // this is a static data
+            currBalance = balance;
+        }
+
+        // Static members to get/set interest rate.
+        public static void SetInterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
